dc726583e7e46f8d6421cf50951dc38ab6984263
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-06-25 16:41:43 +0000

    new codes for python

---
+++ python-hello_world/2-print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+#a python script that prints a string
+
+print('Programming is like building a multilingual puzzle')
---
+++ python-hello_world/README.md
@@ -0,0 +1,2 @@
+#This is a readme
+This repo contains scripts written in python language
