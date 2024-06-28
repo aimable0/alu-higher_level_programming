d9449528df3aed6c9b373ab29fcd8449ae1fe9b5
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-06-28 18:39:53 +0000

    code

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ if number < 0:
     number = -1 * number
     neg_number = -1 * (number % 10)
     if neg_number < 6 and neg_number != 0:
-        print (f"Last digit of {number * -1} is {neg_number} and is less than 6 and not 0")
+        print(f"Last digit of {number * -1} is {neg_number} and is less than 6 and not 0")
 else:
-    last_digit = number  % 10
+    last_digit = number % 10
     if last_digit > 5:
         print(f"Last digit of {number} is {last_digit} and is greater than 5")
     if last_digit == 0:
