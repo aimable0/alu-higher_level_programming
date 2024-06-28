fa1c89d31b829ba5268f42d47fa6532b4c9fa67c
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-06-28 21:50:40 +0000

    codes for python_if_else

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -3,7 +3,9 @@ def print_last_digit(number):
     if number < 0:
         number = -1 * number
         last_digit = number % 10
+        print(last_digit)
         return last_digit * -1
     else:
         last_digit = number % 10
+        print(last_digit)
         return last_digit
