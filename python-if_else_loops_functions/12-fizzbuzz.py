ffab4e19b078c4feccff43ba27da493a0ce5f26e
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-06-28 21:30:59 +0000

    codes for python_if_else

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print('FizzBuzz ', end="")
-    elif i % 3 == 0:
-        print('Fizz ', end="")
-        #code for multiples of three
-    elif i % 5 == 0:
-        print('Buzz ', end="")
-        #code for multiples of five
-
-        #code for multiples of three and five
-    else:
-        print('{} '.format(i), end="")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz ', end="")
+        elif i % 3 == 0:
+            print('Fizz ', end="")
+        elif i % 5 == 0:
+            print('Buzz ', end="")
+        else:
+            print('{} '.format(i), end="")
