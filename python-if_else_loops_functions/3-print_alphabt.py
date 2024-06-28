5036849df0884784eae66e4088909a112027024b
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-06-28 19:59:30 +0000

    codes for python_if_else

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join( "{:c}".format(i) for i in range(97, 123) if i not in (113, 101)), end = "")
+print("".join( "{:c}".format(i) for i in range(97, 123) if i not in (113, 101)), end="")
