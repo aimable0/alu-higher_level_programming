b7677d7f7ee9d3531c01726f87908e6825f1a2ef
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-06-29 11:24:26 +0000

    codes for imports and modules

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-import add_0
 
-a = 1
-b = 2
-sum_ab = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, sum_ab))
+if __name__ = "__main__"
+    for add_0 import add
+
+    a = 1
+    b = 2
+    sum_ab = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, sum_ab))
