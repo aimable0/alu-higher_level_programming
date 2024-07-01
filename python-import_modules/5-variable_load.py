3d4417a881d6f8f41cb15504b53cdbde5c11867c
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-01 21:03:28 +0000

    codes for imports and modules

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from variable_load_5.py import returns_var
+    from variable_load_5 import returns_var
     
-    print("{}".format(returns_var()))
+    value = returns_var()
+    print("{}".format(value))
