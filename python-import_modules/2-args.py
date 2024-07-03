2fd999d3c7d59cd9776c5fad8fde1836b66dcdaa
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-03 02:34:10 +0000

    codes for imports and modules

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    
+    import sys
+
+    total_args = len(sys.argv)
+    arg_num = 1
+
+    if total_args == 0:
+        print("0 arguments")
+    else:
+        print(f"{total_args - 1} arguments:")
+        for i in range(total_args):
+            if i > 0:
+                print("{}: {}".format(arg_num, sys.argv[i]))
+                arg_num = arg_num + 1
