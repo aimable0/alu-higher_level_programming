c19b83e734fa260d4a85694d3f34e44d9e8f855d
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-03 00:36:37 +0000

    codes for imports and modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)    
+if __name__ == "__main__":
+    import hidden_4.pyc
+
+    file_name = "hidden_4.pyc"
+
+    with open(file_name, 'r') as file:
+        for line in file:
+            if line[:2] != "__"
+                print(line)
