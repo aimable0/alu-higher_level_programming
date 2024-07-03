33af609f62047cc872bc136568e1aad01be6f0dc
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-03 03:56:29 +0000

    codes for imports and modules

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == "__main__":  # condition for: only runs as main not when imported
     import sys
 
     tot_num = len(sys.argv)
     sum = 0
 
-    if tot_num == 1: #for one argument which is the script_name
+    if tot_num == 1:  # for one argument which is the script_name
         sum = 0
-    elif tot_num == 2: #for two argument meaning a number and the script_name
+    elif tot_num == 2:  # for two argument meaning a number and the script_name
         num = f"{sys.argv[1]}"
-        sum = int(num) #we just return the number
+        sum = int(num)  # we just return the number
     else:
-        for i in range(tot_num): #for more than 2 arguments
+        for i in range(tot_num):  # for more than 2 arguments
             if i > 0:
-                str_num = f"{sys.argv[i]}" #collect the number on each iteration
-                int_num = int(str_num) #cast the number from string to int format
-                sum = sum + int_num #add it to the sum on each iteration
+                str_num = f"{sys.argv[i]}" # collect the number on each iteration
+                int_num = int(str_num)  # cast the number from string to int format
+                sum = sum + int_num  # add it to the sum on each iteration
     print(sum)
