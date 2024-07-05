88b637d18dde127e706497212d525b883bbda6b9
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-05 03:06:58 +0000

    codes for lists and tuples

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list[0] != " ":
-        for number in sorted(my_list, reverse=True):
-            print("{:d}".format(number))
-    else:
-        return None
+    if my_list == None
+        return
+    for number in sorted(my_list, reverse=True):
+        print("{:d}".format(number))
