24afccdb0fd66262f0ac65b9d6a2280ee866bfd3
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-06 00:43:00 +0000

    codes for lists and tuples

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(var):
     new_string = ""
     for letter in var:
-       if letter != 'c' and letter != 'C':
-           new_string += letter
+        if letter != 'c' and letter != 'C':
+            new_string += letter
     return new_string
