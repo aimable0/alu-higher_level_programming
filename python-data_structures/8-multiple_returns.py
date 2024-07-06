78e0b70e8f8efd6243ea8ce17de59971047a5305
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-06 01:55:24 +0000

    codes for lists and tuples

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = 0
-    for letter in sentence:
-        length = length + 1
-    char_1 = sentence[0]
-    len_and_char1 = (length, char_1)
-    return len_and_char1
+    if sentence != "":
+        for letter in sentence:
+            length = length + 1
+        char_1 = sentence[0]
+        len_and_char1 = (length, char_1)
+        return len_and_char1
+    else:
+        length = 0
+        char_1 = None
+        len_and_char1 = (length, char_1)
+        return len_and_char1
