4d6a4938b62f51829e91f4c48988a6c62e93151b
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-09 20:25:35 +0000

    codes for lists and tuples

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    loop = 0
-    for each_matrix in matrix:
-        for item in each_matrix:  
-            loop = loop + 1
-            if loop == 3:
-                print("{:d} ".format(item))
-            else:
-                print("{:d} ".format(item), end="")
-        loop = 0
+    i = 0
+    for list in matrix:
+        for item in list:
+            if i == (len(list) - 1):
+                print('{}'.format(item))
+                continue
+            print('{} '.format(item), end="")
+            i = i + 1
+        i = 0
