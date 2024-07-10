e1477e01476c3836ff05072ad54f550d97a81fc9
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-10 12:32:36 +0000

    codes for data structres

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for item in matrix:
         new_matrix.append(list(map(lambda x: x * x, item)))
-    return new_matrix
 
+    return new_matrix
