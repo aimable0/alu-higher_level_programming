3b0139ae3688eb68310d4579b9f996fa9a0aa75f
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-13 21:53:27 +0000

    codes

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """doc string"""
 
+
 def inherits_from(obj, a_class):
     """this check for inheritance of an object or not"""
     return isinstance(obj, a_class) and type(obj) != a_class
