aff07fcf9679bcdf967608f7e2a6d117e0642cdb
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-13 21:36:00 +0000

    codes

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module contains afunction that returns a list of atts and meths"""
 
+
 def lookup(obj):
     """This function returns a list of all atts and meths of a class"""
     return dir(obj)
