b62094d6c52d6dc232b9e7ba851487cc9c852be3
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-13 16:13:15 +0000

    new codes with classes

---
+++ python-classes/6-square.py
@@ -0,0 +1,45 @@
+class Square:
+    """This class represents a sqaure"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """initiation of attributes"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self._size = value
+        else:
+            raise TypeError('size must be na integer')
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple:
+            self._position = value
+        else:
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+
+    def area(self):
+        return self._size * self._size
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print("")
+            for i in range(self.size):
+                print(' ' * self.position[0] + '#' * self.size)
