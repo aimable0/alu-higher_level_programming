ebdcc9209e7aa83fff44c32745b879e6ddc850ad
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-13 13:55:16 +0000

    new codes with classes

---
+++ python-classes/2-sqaure.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module contains a an expanded Square class"""
+
+
+class Square:
+    """This class defines a square with private datas"""
+    
+    def __init__(self, size=0):
+        """initiates Square attributes"""
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be an integer')
---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module contains a an expanded Square class"""
+
+
+class Square:
+    """This class represents a sqaure"""
+
+    def __init__(self, size=0):
+        """initiation of attributes"""
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
+        else:
+            raise TypeError('size must be na integer')
+    
+    def area(self):
+        return self.__size * self.__size
