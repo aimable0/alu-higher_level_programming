1377c67882d330e3468ff0ba110f64bf6bac4195
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-13 18:48:07 +0000

    more simple projects for classes

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This module contains a class that represents a rectangle"""
+
+
+class Rectangle:
+    """this class represents a simple rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self._width = value
+        else:
+            raise TypeError('width must be an integer') 
+
+    @property
+    def height(self):
+        return self._height
+    
+    @height.setter
+    def height(self, value):
+        if type(value) is int:
+            if value < 0:
+                raise  ValueError('height must be an integer')
+            else:
+                self._height = value
+        else:
+            raise TypeError('height must be an integer')
