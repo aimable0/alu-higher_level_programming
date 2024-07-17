e107d1f0cff5fb87b39c675148caa69a80a44fb9
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-17 00:35:03 +0000

    more simple projects for classes

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if type(value) is not int:
-            raise TypeError('width must be an nteger')
+            raise TypeError('width must be an integer')
         else:
             if value < 0:
                 raise ValueError('width must be >= 0')
