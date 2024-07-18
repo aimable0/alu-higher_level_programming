d7e8af782938371f4a58b8beea21cdf874dabcc3
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-18 12:54:07 +0000

    codes

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a simple mode of a list"""
+
+class MyList(list):
+    """doc string"""
+
+    def print_sorted(self):
+        """prints a sorted list"""
+        print(sorted(self))
+
