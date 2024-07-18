c112eb97ffc1cf5c3a93470b91dd5d4680241254
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-18 21:23:15 +0000

    codes

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""doc string"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """this is a square class that inherits from class rectangle
+    attributes
+    size: size of the square
+    methodes
+    area: which return the square of size
+    """
+
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        self._size = size
+
+    def area(self):
+        return self._size ** 2
+
+    def __str__(self):
+        return f"[Square] {self._width}/{self._height}"
