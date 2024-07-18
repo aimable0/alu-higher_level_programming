2267d60079ac8e02da0d1215201abdfb696f6818
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-18 20:36:48 +0000

    codes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(BaseGeometry):
     """a rectangle class that inherits from BaseGeometry"""
 
     def __init__(self, width, height):
+        """attribute initiation"""
         self.integer_validator("height", height)
         self._height = height
         self.integer_validator("width", width)
