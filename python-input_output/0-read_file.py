ddf23ea01be2bc17c85b48bf409a9bd94a1632e2
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-21 20:38:00 +0000

    files input and output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
+
 """This contains a file opening and reading function"""
-
-
 def read_file(filename="")
     """this function opens and reads a file"""
 
     with open(filename, encoding="utf-8") as f:
+        """doc string"""
         read_data = f.read()
         print(read_data)
