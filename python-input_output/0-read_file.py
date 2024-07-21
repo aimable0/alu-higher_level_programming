0e38a34b603b13184c3b92e712d24378e08bbe22
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-07-21 20:41:49 +0000

    files input and output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,11 +2,11 @@
 """
 Module 0-read_file
 
-contains a file opening and reading function
+Contains a file opening and reading function
 """
 
 
 def read_file(filename="")
-    """this function opens and reads a file"""
+    """Read and print text from file"""
     with open(filename, encoding="utf-8") as f:
         print(f.read().strip())
