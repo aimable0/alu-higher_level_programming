e112f4c2d296c88d692460ed6b564bf2f6a05ccd
aimable0 <aimablennkurikiyimana1@gmail.com>
2024-09-24 17:36:08 +0000

    js projects

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+const args = process.argv.slice(2); //this stores in an array the arguments pass on the command line
+
+console.log(args[0], "is", args[1]);
---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const numArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", 
+	"0"];
+
+//check if string is number or not
+if (args.length >= 1) {
+	let arg1 = args[0]
+
+	if (arg1[0] in numArr) {
+		console.log(parseInt(args[0]))
+	} else {
+		console.log('Not a number')
+	}
+} else {
+	console.log("Not a number")
+}
