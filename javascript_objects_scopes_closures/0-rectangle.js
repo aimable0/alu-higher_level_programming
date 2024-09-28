f85a44c90267baec5af557b0b4eb960f3582ca2f
aimable0 <aimablennkurikiyimana1@gmail.com>
2024-09-28 17:27:01 +0000

    adding more codes

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor () {
-    this.width = 0;
-    this.height = 0;
-  }
 }
-const rectangle = new Rectangle();
-console.log(rectangle);
+module.exports = Rectangle; // this exports the class Rectangle to be visible outside of this file
