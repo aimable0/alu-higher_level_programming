bc61af215c22c166d6bd263bf308cadcd65c1aab
aimable0 <aimablennkurikiyimana1@gmail.com>
2024-09-28 22:25:05 +0000

    adding new codes

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -4,7 +4,8 @@ const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
   constructor (_size) {
     super(_size, _size); // we are calling the Rectangle constructor..
-    this.size = _size; // after calling the superclass we can now do a reference.
+    this.height = _size; // after calling the superclass we can now do a reference.
+    this.width = _size;
   }
 }
 module.exports = Square;
