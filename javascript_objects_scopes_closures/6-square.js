a92dd8f936d4d046a59d23f6c9fa15bebea27037
aimable0 <aimablenkurikiyimana1@gmail.com>
2024-09-29 23:29:09 +0200

    trying to fix

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,23 +2,16 @@
 const SquareParent = require('./5-square');
 
 class Square extends SquareParent {
-  charprint(c) {
+  charPrint(c) {
+    // If no character is provided, default to 'X'
     if (c === undefined) {
-      for (let i = this.height; i > 0; i--) {
-        for (let m = this.width; m > 0; m--) {
-          process.stdout.write('X');
-        }
-        console.log();
-      }
-    } else {
-      for (let i = this.height; i > 0; i--) {
-        for (let m = this.width; m > 0; m--) {
-          process.stdout.write(c);
-        }
-        console.log();
-      }
+      c = 'X';
     }
 
+    // Loop to print each row of the square
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 
 }
