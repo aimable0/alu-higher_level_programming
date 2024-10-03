b246b6cf0e30088038191be4365b55365ed24274
aimable0 <aimablenkurikiyimana1@gmail.com>
2024-10-03 02:27:04 -0700

    adding a new code

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const fileName = process.argv.slice(2)[0];
+let content = process.argv.slice(2)[1];
+const fs = require('fs');
+fs.writeFile(fileName, content, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+})
