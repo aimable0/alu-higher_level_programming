9b958aa91a37600852ed782c111680a25262d7ca
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-05 16:27:24 +0200

    fixed

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
-#!/usr/bin/node 
+#!/usr/bin/node
 const request = require('request');
 
 const url = process.argv.slice(2)[0];
 request(url, (error, response, body) => {
-    console.log("code:", response.statusCode);
+  console.log("code:", response.statusCode);
 })
