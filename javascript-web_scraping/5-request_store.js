997c5d955f4cebc6562f018257e01141d9264780
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-05 21:58:38 +0200

    fixing semistandard

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,13 +1,13 @@
 const request = require('request');
 const fs = require('fs');
-const fileName  = process.argv.slice(2)[1];
+const fileName = process.argv.slice(2)[1];
 const url = process.argv.slice(2)[0];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.log(err);
-    } else {
-        fs.writeFile(fileName, body, (err) =>
-            console.log(err))
-    }
-})
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(fileName, body, (err) =>
+      console.log(err));
+  }
+});
