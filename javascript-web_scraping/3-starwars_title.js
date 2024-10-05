125d45044cc5dc70f8d96e9b7fde723e5e848f98
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-05 20:08:09 +0200

    semistandard

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,10 +4,10 @@ const request = require('request');
 let index = process.argv.slice(2)[0];
 index -= 1;
 request('https://swapi-api.alx-tools.com/api/films/', (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const data = JSON.parse(body);
-        console.log(data.results[index].title);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.results[index].title);
+  }
 })
