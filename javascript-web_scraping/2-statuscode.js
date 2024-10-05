ab818cb7be8550c35dc2951b9aa617d65adb79fd
aimable0 <aimablenkurikiyimana1@gmail.com>
2024-10-05 07:08:32 -0700

    fixing a  web scrap

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 const url = process.argv.slice(2)[0]
-request('url', (error, response,body) => {
+request(url, (error, response,body) => {
   if(!error && response.statuCode === 200) {
     console.log("code:", response.statusCode);
   } else {
