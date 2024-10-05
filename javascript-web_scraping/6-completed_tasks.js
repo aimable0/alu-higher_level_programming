fb93520cd7f47d4e68f976b2605114f8abdac0ef
Nkurikiyimana Aimable <aimablenkurikiyimana1@gmail.com>
2024-10-06 00:01:35 +0200

    p

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,23 +1,21 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv.slice(2)[0]
+const url = process.argv.slice(2)[0];
 
 request(url, (error, response, body) => {
-  
   if (error) {
     console.error(error);
   } else {
-    let arr = {}
+    const arr = {};
     const data = JSON.parse(body);
     for (let i = 0; i < data.length; i++) {
-        if (data[i].userId in arr && data[i].completed === true) {
-            arr[data[i].userId] += 1; 
-        } else if (data[i].completed === true){
-            arr[data[i].userId] = 1;
-        }
+      if (data[i].userId in arr && data[i].completed === true) {
+        arr[data[i].userId] += 1;
+      } else if (data[i].completed === true) {
+        arr[data[i].userId] = 1;
+      }
     }
     console.log(arr);
   }
-
-})
+});
